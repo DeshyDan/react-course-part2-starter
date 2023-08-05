0db2fc306680ef27ba3ac6f073da28e83d745d04
Deshy Dan <deshy733@gmail.com>
2023-08-05 10:37:29 +0200

    Added logic for mutating data

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,38 +1,56 @@
-import { useMutation } from '@tanstack/react-query';
-import { useRef } from 'react';
-import { Todo } from '../hooks/useTodo';
-import axios from 'axios';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "../hooks/useTodo";
+import axios from "axios";
 
 const TodoForm = () => {
-  const addTodo = useMutation({
-    mutationFn:(todo:Todo) =>
-    axios
-  .post("https://jsonplaceholder.typicode.com/todos", todo)
-  .then(res => res.data)
-  })
-  const ref = useRef<HTMLInputElement>(null);
+    const queryClient = useQueryClient();
 
-  return (
-    <form className="row mb-3" onSubmit={e =>{
-      e.preventDefault();
-      if(ref.current && ref.current.value)
-       { addTodo.mutate({
-          id:0,
-          title:ref.current?.value,
-          completed:false,
-          userId:1
-        })}
+    const addTodo = useMutation({
+        mutationFn: (todo: Todo) =>
+            axios
+                .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+                .then((res) => res.data),
+        onSuccess: (savedTodo, newTodo) => {
+            //  approach : invalidating the cache
 
+            // queryClient.invalidateQueries({
+            //     queryKey: ["todo"],
+            // });
+            // APPROACH 2: UPDATING THE DATA IN THE CACHE
+            queryClient.setQueriesData<Todo[]>(["todo"], (todos) => [
+                savedTodo,
+                ...(todos || []),
+            ]);
 
-    }}>
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
-  );
+            console.log(savedTodo);
+        },
+    });
+    const ref = useRef<HTMLInputElement>(null);
+
+    return (
+        <form
+            className="row mb-3"
+            onSubmit={(e) => {
+                e.preventDefault();
+                if (ref.current && ref.current.value) {
+                    addTodo.mutate({
+                        id: 0,
+                        title: ref.current?.value,
+                        completed: false,
+                        userId: 1,
+                    });
+                }
+            }}
+        >
+            <div className="col">
+                <input ref={ref} type="text" className="form-control" />
+            </div>
+            <div className="col">
+                <button className="btn btn-primary">Add</button>
+            </div>
+        </form>
+    );
 };
 
 export default TodoForm;
